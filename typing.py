3b1cf8be15c5c1295c2993a367e9befa9f0d37c9
demonavatar <av.demon@gmail.com>
2022-08-25 10:16:04 +0300

    added typing2

--- typing.py
+++
@@ -1,9 +0,0 @@
-from typing import Union
-
-
-# Аргумент x может принимать целое число или строку
-def hundreds(х: Union[int, str]) -> str:
-    return str(х * 100)
-
-hundreds(100)
-hundreds('сто') 
---
+++ typing2.py
@@ -0,0 +1,38 @@
+# case 9.2.1 
+
+# from typing import Sequence
+# def lower_join(seq:Sequence[str]) -> str:
+#     """Принимает на вход последовательность и создаёт из неё  
+#     строку в нижнем регистре."""
+#     return ''.join(seq).lower()
+
+# case 9.2.2
+
+# from typing import List,Union
+# def series_sum(incoming:List[Union[str,float]]) -> str:
+#     """Принимает на вход список из строк и чисел с плавающей точкой, приводит его элементы к строкам и конкатенирует их.
+#     """
+#     result = ''
+#     for i in incoming:
+#         result += str(i)
+#     return resultS
+
+# case 9.2.2
+
+from typing import Callable
+
+# def add(number:float, callback:Callable[[float],float]) -> float:
+#     """Производит арифметические действия с числами.
+#     Принимает число и функцию, выполняющую арифметическое действие.
+#     """    
+#     return callback(number)
+
+
+# def adder20(number:float) -> float:
+#     """Добавляет к аргументу 20."""
+#     return number + 20
+
+
+# def multiplier2(number:float) -> float:
+#     """Умножает аргумент на 2."""
+#     return number * 2
